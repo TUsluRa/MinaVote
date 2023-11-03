5d5b28b36462583152557f00393c05aa5549b5e0
TUsluRa <149424293+TUsluRa@users.noreply.github.com>
2023-11-04 02:53:42 +0300

    Update Css, Homepage

--- mina-vote/src/Pages/Home/HomePage.tsx
+++ mina-vote/src/Pages/Home/HomePage.tsx
@@ -1,34 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './styles.css';
+  
 
-function HomePage() {
+
+  const HomePage: React.FC = () => {
+    const [votesData, setVotesData] = useState([]); 
     return (
         <div className="container">
             <div className="header">
                 <div className="avatar"></div>
                 Mina Vote
-                <button className="button">Connect button</button>
+                <button className="button">Connect Wallet</button>
             </div>
 
-            <div className="vote-section">
-                <div className="vote-slider">
-                    <div className="vote-card"></div>
-                    <div className="vote-card"></div>
-                    <div className="vote-card"></div>
+            <div className='MainContent'>
+                <div className="votes-section-slider">
+                    <div className="slider">
+                        <div className="vote-slider-card">
+                            <div className="vote-slider-card-title">Vote Title 1</div>
+                            <div className="vote-slider-card-description">Description of the vote.</div>
+                        </div>
+                        <div className="vote-slider-card">
+                        <div className="vote-slider-card-title">Vote Title 2</div>
+                            <div className="vote-slider-card-description">Description of the vote.</div>
+                        </div>
+                        <div className="vote-slider-card">
+                        <div className="vote-slider-card-title">Vote Title 3</div>
+                            <div className="vote-slider-card-description">Description of the vote.</div>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="current-votes">
-                    Last Votes List item 1
-                    Last Votes List item 2
-                    Last Votes List item 3
-                </div>
-
-                <div className="top-votes">
-                    Top Votes
+                <div className='Vot-Modul-1'>
+                    <div className="votes-section-list">
+                        <div className="votes-list">
+                        {votesData && votesData.map((vote) => (
+                                <div key={vote.id} className="votes-list-item">
+                                    {vote.Vote_Image && (
+                                        <img src={vote.Vote_Image} alt={vote.Vote_Name} className="list-image" />
+                                    )}
+                                    <div className="list-content">
+                                        <h2 className="list-title">{vote.Vote_Name}</h2>
+                                        <p className="list-description">{vote.Vote_Content}</p>
+                                        <p className="list-date">End Date: {vote.Vote_EndDate}</p>
+                                    </div>
+                                    <label className="list-vote-label">
+                                        Vote
+                                    </label>
+                            </div>
+                                ))}
+                        </div>
+                    </div>
+                    <div className="votes-section-top">
+                        <div className="top-votes">                            
+                            <div className="top-votes-item">
+                                <div className="top-votes-profile"></div>
+                                <div className="top-votes-content"><p>Test-1</p></div>
+                                <div className="top-votes-status"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    );
-}
-
+        );
+    };    
 export default HomePage;
